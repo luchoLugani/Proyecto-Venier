b766fd24c0e9681eb4a5a8de8f27b206d8b6f54e
luciano lugani <lvchx_01@hotmail.com>
2019-10-29 21:20:10 -0300

    listo

---
+++ Cliente/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+
+            listen.Connect(connect);
+
+            byte[] enviar_info = new byte[100];
+            string data;
+            Console.WriteLine("-Ingrese la info a enviar");
+            data = Console.ReadLine();
+
+            enviar_info = Encoding.Default.GetBytes(data);
+
+            listen.Send(enviar_info);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejemploClienteServidor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ejemploClienteServidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket conexion;
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+
+            listen.Bind(connect);
+            listen.Listen(10);
+
+            conexion = listen.Accept();
+            Console.WriteLine("conexion aceptada :v");
+
+            byte[] recibir_info = new byte[100];
+            string data = "";
+            int array_size = 0;
+
+            array_size=conexion.Receive(recibir_info, 0, recibir_info.Length, 0);
+
+            Array.Resize(ref recibir_info, array_size);
+            data = Encoding.Default.GetString(recibir_info);
+
+            Console.WriteLine("la info recibida es: {0}", data);
+            Console.ReadKey();
+
+
+        }
+    }
+}
